760fdd5152323283be493cf0ba1ef6741c30f6f1
deborah-code <dbrhadewunmi@gmail.com>
2021-07-19 12:59:53 +0100

    installed react-icons

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -10,7 +10,7 @@ export function handleInitialData() {
     return getInitialData().then(({ users, tweets }) => {
       dispatch(receiveUsers(users));
       dispatch(receiveTweets(tweets));
-      dispatch(setAuthedUser)(id);
+      dispatch(setAuthedUser(AUTHED_ID));
     });
   };
 }
